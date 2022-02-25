2e494be2180c86e7ddb3fdbacc8e53fa8f03b8b9
EgorPrivzogrin <98107069+EgorPrivzogrin@users.noreply.github.com>
2022-02-25 20:24:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-if __name__=="__main__":
-  from math import sqrt
-a =int(input("коэффицент а"))
-b =int(input("коэффицент b" ))
-c =int(input("коэффицент c"))
-D = b ** 2 - (4 * a * c)
-if D < 0:
-    print('Корней нет')
-elif D > 0:
-    x1=((-b + sqrt(D)) / (2 * a)
-    x2=((-b - sqrt(D)) / (2 * a)
-    print(x1, x2)
-elif D == 0:
-    print(-b / (2 * a))
+if __name__ == '__main__':
+    a = float(input('коэффицент a:'))
+    b = float(input('коэффицент b:'))
+    c = float(input('коэффицент c:'))
+    D = b ** 2 - (4 * a * c)
+    if D < 0:
+        print('Корней нет')
+    elif D > 0:
+        x1 = (-b + D ** (1 / 2)) / (2 * a)
+        x2 = (-b - D ** (1 / 2)) / (2 * a)
+        print(x1, x2)
+    else:
+        print(-b / (2 * a))
